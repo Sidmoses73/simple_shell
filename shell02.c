8e1b38e44a19ee82392d6682b1b6e36c17df2083
Sidmoses73 <sidmoses73@gmail.com>
2023-04-26 18:03:12 -0700

    Simpleshell02

---
+++ shell02.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int shell_main(void)
+{
+pid_t pid;
+
+pid = fork();
+
+if (pid == -1)
+{
+perror("fork");
+exit(EXIT_FAILURE);
+}
+else if (pid == 0)
+{
+/*child process code here*/
+}
+else
+{
+/*parent process code here*/
+}
+return (0);
+}
